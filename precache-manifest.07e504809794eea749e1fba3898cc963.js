bf71d0e7a2319eca4a36ad55871ab11114ae0b8c
BrisiaCastrejon <brissmonse@gmail.com>
2018-12-04 07:24:03 -0600

    Updates

---
+++ precache-manifest.07e504809794eea749e1fba3898cc963.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2ddacedc6287ea88b63",
+    "url": "/Generation/static/js/runtime~main.a2ddaced.js"
+  },
+  {
+    "revision": "38ddd2fc9d7c181ad172",
+    "url": "/Generation/static/js/main.38ddd2fc.chunk.js"
+  },
+  {
+    "revision": "1bbb5848dd6d1f77fd29",
+    "url": "/Generation/static/js/1.1bbb5848.chunk.js"
+  },
+  {
+    "revision": "38ddd2fc9d7c181ad172",
+    "url": "/Generation/static/css/main.4fdc0e9f.chunk.css"
+  },
+  {
+    "revision": "4c1213a9617337bc3510d50fde8c9a0e",
+    "url": "/Generation/index.html"
+  }
+];
